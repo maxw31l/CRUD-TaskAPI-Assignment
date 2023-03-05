ac6e76fd2d8f32ff74329da4f5a56f3b8363efa9
Egidijus <vaitkeviciusegis@gmail.com>
2023-03-06 00:07:26 +0200

    Added cell to tableView in UpdateTaskViewController

--- APITask-Egidijus/APITask-Egidijus/UpdateTaskCell.swift
+++ APITask-Egidijus/APITask-Egidijus/UpdateTaskCell.swift
@@ -1,23 +1,8 @@
 //
-//  UpdateTaskCellTableViewCell.swift
+//  UpdateTaskCell.swift
 //  APITask-Egidijus
 //
-//  Created by Egidijus Vaitkevičius on 2023-03-05.
+//  Created by Egidijus Vaitkevičius on 2023-03-06.
 //
 
-import UIKit
-
-class UpdateTaskCellTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
-}
+import Foundation
