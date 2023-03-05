2baeffa47cc2705b4461d64e5883e0cac86f5723
Egidijus <vaitkeviciusegis@gmail.com>
2023-03-05 23:30:15 +0200

    Some changes in UpdateTaskVC UI

---
+++ APITask-Egidijus/APITask-Egidijus/UpdateTaskCell.swift
@@ -0,0 +1,23 @@
+//
+//  UpdateTaskCellTableViewCell.swift
+//  APITask-Egidijus
+//
+//  Created by Egidijus Vaitkevičius on 2023-03-05.
+//
+
+import UIKit
+
+class UpdateTaskCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
