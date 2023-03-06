28469fb217e32a5d11530f9e220281e80374f75d
Egidijus <vaitkeviciusegis@gmail.com>
2023-03-06 12:07:19 +0200

    Added text fields to UpdateTaskViewController View

---
+++ APITask-Egidijus/APITask-Egidijus/Cells/DescriptionCell.swift
@@ -0,0 +1,8 @@
+//
+//  DescriptionCell.swift
+//  APITask-Egidijus
+//
+//  Created by Egidijus Vaitkevičius on 2023-03-06.
+//
+
+import Foundation
---
+++ APITask-Egidijus/APITask-Egidijus/Cells/TitleViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TitleViewCell.swift
+//  APITask-Egidijus
+//
+//  Created by Egidijus Vaitkevičius on 2023-03-06.
+//
+
+import UIKit
+
+class TitleViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
